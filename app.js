0f50d07dc608f0ad1ac8783d030ddce3d5460313
Irfan <irfanalamt@gmail.com>
2020-10-01 00:37:03 -0400

    static folder set

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT|| 5000;
-app.get('/',(req,res)=>{
-res.send('<h1>Hello world again!</h1>')
-});
+var path = require("path");
+
+const logger = (req,res,next)=>{
+    console.log(`${req.protocol}||${req.process}`);
+    next();
+};
+app.use(logger);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
