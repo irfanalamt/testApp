bbea4ce6b56ef2d0f0e2bc8af226a2a2412ab33a
Irfan <irfanalamt@gmail.com>
2020-10-03 23:56:56 -0400

    code cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>res.render('welcome'));
+router.get('/',(req,res)=>res.render('index'));
+router.get('/loginPage',(req,res)=>res.render('login'));
 
 
 
