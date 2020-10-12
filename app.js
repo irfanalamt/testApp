96402677bb05fa74a3fb0e17b2c5c1669cadad85
Irfan <irfanalamt@gmail.com>
2020-10-12 00:18:56 -0400

    learning authentication,mongoose

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT|| 5000;
 var path = require("path");
 //const mongoose = require('mongoose');
 const expressLayouts = require('express-ejs-layouts');
+const { urlencoded } = require('express');
 
 //logger
 const logger = (req,res,next)=>{
@@ -14,6 +15,7 @@ app.use(logger);
 
 app.use(expressLayouts);
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended:false}));
 
 //routes
 app.use('/',require('./routes/index'))
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/',(req,res)=>res.render('index'));
 router.get('/loginPage',(req,res)=>res.render('login'));
+router.get('/register',(req,res)=>res.render('register'));
 
 
 
